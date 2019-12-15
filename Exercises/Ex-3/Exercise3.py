1b4018385c53c98ef3b438050c8a71704c595050
Pablo Cañal <pablocsmw3@gmail.com>
2019-12-15 20:08:08 +0100

    Ex3 done

--- Exercises/Ex-3/Exercise3.py
+++ Exercises/Ex-3/Exercise3.py
@@ -18,36 +18,67 @@ import sys
 
 def main():
 
-    validatedDrugs = getDrugs()
+##    validatedDrugs = getDrugs()
     es = Elasticsearch()
+
+    results = es.search(
+        index="reddit-mentalhealth4",
+        body = {
+          "query": {
+            "query_string": {
+              "default_field": "selftext",
+              "query": "prescribed|mg|mL"
+            }
+          },
+          "aggs": {
+            "Terminos mas significativos": {
+              "significant_terms": {
+                "field": "selftext",
+                "gnd": {},
+                "size":3000
+              }
+            }
+          }
+        },
+        request_timeout=50
+    )
 
     count=0
     for doc in results["aggregations"]["Terminos mas significativos"]["buckets"]:
-               if(validate(doc["key"],validatedDrugs)):
-                    count=count+1
-                    print ("VALIDADO:"+doc["key"])
-    print (count)
+        if(validate(doc["key"])):
+            count+=1
+            print ("VALIDADO:"+doc["key"])
+    print(count)
+    print("Program terminated.")
 
-def getDrugs():
-    sparql = SPARQLWrapper("https://query.wikidata.org/sparql")
-    sparql.setQuery("""
-       SELECT ?moleculeLabel
-    WHERE
-    {
-    	?molecule  wdt:P31 wd:Q12140
-    	; wdt:P274 ?formule
-    	; wdt:P117 ?picture
-    	SERVICE wikibase:label {  bd:serviceParam wikibase:language "en, de" . }
-    }
-    ORDER BY ?moleculeLabel
-    """)
-    sparql.setReturnFormat(JSON)
-    return sparql.query().convert()
 
-def validate(name,results):
-    for result in results["results"]["bindings"]:
-        if(result["moleculeLabel"]["value"]==name):
-            return True
+def validate(keyword):
+    import requests
+
+    q12140 = 'Q12140'
+    p31 = 'P31'
+
+    r = requests.get('https://www.wikidata.org/w/api.php?action=wbsearchentities&search='+keyword+'&language=en&format=json')
+    data = r.json()
+
+    ids = []
+    for x in data["search"]:
+        ids.append(x["id"])
+
+    for id in ids:
+
+        r2 = requests.get('https://www.wikidata.org/w/api.php?action=wbgetentities&ids='+id+'&languages=en&format=json')
+
+        dataCheck = r2.json()
+
+        isValid = dataCheck["entities"][id]["claims"]
+        for item in isValid:
+            if(item == "P31"):
+                for x in isValid["P31"]:
+                    checkedId = x["mainsnak"]["datavalue"]["value"]["id"]
+                    if(checkedId == q12140):
+                        return True
+
     return False
 
 if __name__ == '__main__':
